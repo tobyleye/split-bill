e9f532c69731d07fc9001accd4e035502d081799
Tobi Oyeleye <krisella74@gmail.com>
2021-12-24 01:07:09 +0100

    fix errors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import { TestGesture } from "./components/test_gesture"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
-    {/* <TestGesture /> */}
   </StrictMode>,
   rootElement 
 );
